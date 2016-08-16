dedce780d0bdb0a091a3462bc96c1ab00a574719
Devan Huapaya <huapayadevan@gmail.com>
2016-08-15 22:22:02 -0500

    added flexibility

--- js/main.js
+++ js/main.js
@@ -31,3 +31,10 @@ function init() {
         }
     }
 }
+
+var flexThis = [].slice.call(document.querySelectorAll(".flex")).concat(
+    [].slice.call(document.querySelectorAll(".flex"))
+);
+
+flexThis.forEach(function (elm) {flexibility(elm)});
+console.log("done");
